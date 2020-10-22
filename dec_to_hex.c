c1e9245edc74e07d9172f565c8ba6d8ed77a7432
Fare1216 <60678272+Fare1216@users.noreply.github.com>
2020-10-22 10:41:33 +0900

    add
    10進数から16進数
    16進数から10進数
    への変換プログラムを追加

---
+++ dec_to_hex.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int num;
+    
+    num = 127;
+    
+    printf("%x",num);
+    
+    return 0;
+}
---
+++ hex_to_dec.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int num;
+    
+    num = 0x7f;
+    
+    printf("%d",num);
+    
+    return 0;
+}
